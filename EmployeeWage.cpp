92b263bc2c121509980c342e74234aa07a6caaf0
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-16 17:29:03 +0530

    ADDED : Code to check attendance of Employee

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,1 +1,9 @@
-Welcome
+#include <iostream>
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	int status = ((1 + rand() % 2));
+
+	(status == 1) ? cout << "Employee is Present": cout << "Employee is absent";
+}
