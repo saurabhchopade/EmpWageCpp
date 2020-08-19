a72f889a79eadb360cf544984b7280c74040e74e
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-19 20:00:18 +0530

    ADDED : Code to store wages in file

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+void saveWage(int day, int wage) {
+	fstream fileStream;
+   fileStream.open("WageRecords.txt", ios::out | ios::app);
+   if(fileStream.is_open()) {
+      fileStream << day <<" " << wage << endl;
+      fileStream.close();
+  	}
+}
 
 int main() {
 	const int WAGE_PER_HOUR = 20;
 	const int IS_FULL_TIME = 1;
 	const int IS_PART_TIME = 2;
 	const int WORKING_DAYS = 100;
-	int hour, status, totalWage = 0,totalWorkHours = 0;
+	int hour, status, totalWage = 0,totalWorkHours = 0,dailyWage = 0;
 	srand(time(NULL));
 
 	for(int day = 1; day <= WORKING_DAYS; day++) {
@@ -22,8 +32,10 @@ int main() {
 				hour = 0;
 				break;
 		}
-		totalWage += hour * WAGE_PER_HOUR;
+		dailyWage = hour * WAGE_PER_HOUR;
+		totalWage += dailyWage;
 		totalWorkHours += hour;
+		saveWage(day,dailyWage);
 
 		if(totalWorkHours == 100) {
          cout << "\nWage for month = " << totalWage;
